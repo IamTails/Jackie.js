8774d850bbf3d8ef9da980b8b61f3b3c32c16230
Tom <bigt62187+1@gmail.com>
2017-10-01 13:32:58 -0400

    Update Jackie.js

--- Jackie.js
+++ Jackie.js
@@ -88,4 +88,14 @@ bot.on('message', msg => {
       logger.warn(err);
    }
 });
+bot.getRandomColor = () => {
+
+        let letters = '0123456789';
+        let color = '';
+        for (let i = 0; i < 7; i++) {
+            color += letters[Math.floor(Math.random() * 10)];
+        }
+
+        return color;
+    };
 bot.login(config.keys.token);
