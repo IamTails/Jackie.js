c3630326799b4a569feec70294870b28d99e5ada
Tom <bigt62187+1@gmail.com>
2017-10-01 13:33:43 -0400

    Update neko.js

--- commands/neko.js
+++ commands/neko.js
@@ -3,6 +3,7 @@ exports.run = async (bot, msg) => {
     await snekfetch.get('https://nekos.life/api/neko')
         .then(r => message.channel.send({
             embed: {
+                color: bot.getRandomColor(),
                 author: {
                     name: "Nekos \\o/",
                     icon_url: client.user.avatarURL
